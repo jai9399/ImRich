ea050157a2e2f28ccf7ddea58aa839712048b1fe
unknown <jai9399@gmail.com>
2020-01-10 23:10:34 +0530

    Second

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget{
               title:Center(child:Text("I'm Rich")),
               backgroundColor: Colors.blueGrey[900],
             ),  
-            body: Image(
-              image:NetworkImage('https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500')
+            body:Center(child:Image(
+              image:AssetImage('images/diamond.png')
+            ),
             ),
             ),
           );
